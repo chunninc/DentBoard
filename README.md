67baa3bbee98238bf0e2676903e1f0bb6b926971
ChangChunNing <changchunning@ChangChunNings-MacBook-Pro.local>
2015-05-15 02:41:49 -0400

    CSS fix and bug fixed

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DentBoard
+For this assignment, I used Bootstrap for css and AngularJS for frontend reaction.
+The filter will be on the left, and the result and your request condition will be on the right.
+
+By clicking the filter, the browser will send a http GET request with parameters selected on the left side filter and wait for a jsonp object to return. 
--- js/dentBoard.js
+++ js/dentBoard.js
@@ -3,68 +3,67 @@ angular.module('dentBoardApp', [])
     $http.defaults.useXDomain = true;
     $scope.result = [];
     $scope.start = true;
+    $scope.cont = [];
+    $scope.error = "";
     $scope.submit = function() {
       $scope.start = false;
       $scope.attribute_list = [];
+      $scope.cont = [];
+      $scope.err = "";
       $scope.url_string = "http://www.bloomapi.com/api/search/npi?callback=JSON_CALLBACK&key1=credential&op1=eq&value1=DMD";
       
-      if ($scope.zip_code) {
-        $scope.attribute_list.push({key: "practice_address.zip", value: this.zip_code});
-        $scope.zip_code = '';
+      if ($scope.myform.zip_code) {
+        $scope.attribute_list.push({key: "practice_address.zip", value: this.myform.zip_code});
+        $scope.cont.push("Zip Code: " + this.myform.zip_code); 
       }
-      if ($scope.last_name) {
-        $scope.attribute_list.push({key: "last_name", value: this.last_name.toUpperCase()});
-        $scope.last_name = '';
-      }
-
-      if ($scope.first_name) {
-        $scope.attribute_list.push({key: "first_name", value: this.first_name.toUpperCase()});
-        $scope.first_name = '';
+      if ($scope.myform.last_name) {
+        $scope.attribute_list.push({key: "last_name", value: this.myform.last_name.toUpperCase()});
+        $scope.cont.push("Last Name: " + this.myform.last_name.toUpperCase());
       }
 
-      if ($scope.state) {
-        $scope.attribute_list.push({key: "practice_address.state", value: this.state});        
-        $scope.state = '';
+      if ($scope.myform.first_name) {
+        $scope.attribute_list.push({key: "first_name", value: this.myform.first_name.toUpperCase()});
+        $scope.cont.push("First Name: " + this.myform.first_name.toUpperCase());
       }
 
-      if ($scope.city) {
-        $scope.attribute_list.push({key: "practice_address.city", value: this.city.toUpperCase()});
-        $scope.city = '';
+      if ($scope.myform.state) {
+        $scope.attribute_list.push({key: "practice_address.state", value: this.myform.state});        
+        $scope.cont.push("State: " + this.myform.state);
       }
 
-      if ($scope.gender) {
-        $scope.attribute_list.push({key: "gender", value: this.gender});
-        $scope.gender = '';
+      if ($scope.myform.city) {
+        $scope.attribute_list.push({key: "practice_address.city", value: this.myform.city.toUpperCase()});
+        $scope.cont.push("City: " + this.myform.city.toUpperCase());
       }
 
-      if ($scope.npi) {
-        $scope.attribute_list.push({key: "npi", value: this.npi});
-        $scope.npi = '';
+      if ($scope.myform.gender) {
+        $scope.attribute_list.push({key: "gender", value: this.myform.gender});
+        $scope.cont.push("Gender: " + this.myform.gender.toUpperCase());
       }
-      /*
-      for(var i=0; i<$scope.kind_list.length; i++){
-        var tmp = i+2;
-        $scope.url_string += "&key" + tmp + "=" + $scope.kind_list[i];
-        $scope.url_string += "&op" + tmp + "=eq";
-        $scope.url_string += "&value" + tmp + "=" + $scope.value_list[i];
+
+      if ($scope.myform.npi) {
+        $scope.attribute_list.push({key: "npi", value: this.myform.npi});
+        $scope.cont.push("NPI Number: " + this.myform.npi);
       }
-      */
+      for(var key in $scope.myform){
+        $scope.myform[key] = '';
+      }
       for(var i=0; i<$scope.attribute_list.length; i++){
         var tmp = i+2;
         $scope.url_string += "&key" + tmp + "=" + $scope.attribute_list[i].key;
-        if($scope.attribute_list[i].key === "practice_address.zip")
-          $scope.url_string += "&op" + tmp + "=prefix";
-        else
-          $scope.url_string += "&op" + tmp + "=eq";
+        $scope.url_string += "&op" + tmp + "=eq";
         $scope.url_string += "&value" + tmp + "=" + $scope.attribute_list[i].value;
       }
-
+      console.log($scope.url_string);
       $http.jsonp($scope.url_string)
         .success(function(data, status) {
           //$scope.result = data;
           $scope.result = data.result;
-          console.log(data);
-      });
+          //console.log(data);
+      })
+        .error(function(data, status) {
+          $scope.err += status + " error";
+        });
 
     };
 
